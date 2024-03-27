b48742f27e91a70592383396befaafa231acfc08
LlamaUnicorn <holidaynight@gmail.com>
2024-03-27 06:50:43 +0300

    27.03.2024 5.3 Week: 3

--- 01.py
+++ 01.py
@@ -5,12 +5,25 @@
 
 # 5.2 return the n-th Fibonacci num
 # optional: try recursive with memoization
-n = int(input())
-f = [0, 1]
-while len(f) <= n:
-    f.append(f[-1] + f[-2])
-print(f[n])
+# n = int(input())
+# f = [0, 1]
+# while len(f) <= n:
+#     f.append(f[-1] + f[-2])
+# print(f[n])
 
 
-# 5.3
+# 5.3 Manhattan distance
+# x1, x2 = input().split()
+# y1, y2 = input().split()
+# dx = abs(int(x1)-int(x2))
+# dy = abs(int(y1)-int(y2))
+# print(dx, dy)
 
+
+# 5.4
+n, k = map(int, input().split())
+i = max(n // k, 1)
+ans1 = abs(n - k * i)
+i += 1
+ans2 = abs(n - k * i)
+print(min(ans1, ans2))
