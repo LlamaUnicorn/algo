dcb03fa0e2f1db2a961ea3edffd6a550b127912c
LlamaUnicorn <holidaynight@gmail.com>
2024-04-11 06:52:11 +0300

    11.04.2024 6.2 Week: 4

---
+++ 10.py
@@ -0,0 +1,20 @@
+# Наибольший общий делитель. Алгоритм Евклида
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def gcd_recursive(a, b):
+    return gcd(b, a % b) if b else a
+
+
+def gcd_iterative(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+print(gcd(12, 18))
+print(gcd_recursive(12, 18))
+print(gcd_iterative(12, 18))
