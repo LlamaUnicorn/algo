1918864d54d9a7bee6cd35c6c3fb7c1a6ef64722
LlamaUnicorn <holidaynight@gmail.com>
2024-04-13 20:06:47 +0300

    13.04.2024 6.2 Week: 6

--- 10.py
+++ 10.py
@@ -1,28 +1,39 @@
-# Наибольший общий делитель. Алгоритм Евклида
-def gcd(a, b):
-    if b == 0:
-        return a
-    return gcd(b, a % b)
+# # Наибольший общий делитель. Алгоритм Евклида
+# def gcd(a, b):
+#     if b == 0:
+#         return a
+#     return gcd(b, a % b)
+#
+#
+# def gcd_recursive(a, b):
+#     return gcd(b, a % b) if b else a
+#
+#
+# def gcd_iterative(a, b):
+#     while b:
+#         a, b = b, a % b
+#     return a
+#
+#
+# print(gcd(12, 18))
+# print(gcd_recursive(12, 18))
+# print(gcd_iterative(12, 18))
+#
+#
+# # 10.9 НОД нескольких чисел
+# def gcd_list(a):
+#     result = 0
+#     for item in a:
+#         return gcd(result, item)
+#     return result
 
+# 12.3 Объединение отрезков
+def merge_segm(segms):
+    res = []
+    for left, right in sorted(segms):
+        if res and left <= res[-1][1]:
+            res[-1][1] = max(res[-1][1], right)
+        else:
+            res.append([left, right])
+    return res
 
-def gcd_recursive(a, b):
-    return gcd(b, a % b) if b else a
-
-
-def gcd_iterative(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-
-print(gcd(12, 18))
-print(gcd_recursive(12, 18))
-print(gcd_iterative(12, 18))
-
-
-# 10.9 НОД нескольких чисел
-def gcd_list(a):
-    result = 0
-    for item in a:
-        return gcd(result, item)
-    return result
