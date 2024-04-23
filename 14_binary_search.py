68aa47dbed22c3721560a5602ea5ccf010fcb732
LlamaUnicorn <holidaynight@gmail.com>
2024-04-23 06:47:28 +0300

    23.04.2024 14.3 Week: 2

--- 14_binary_search.py
+++ 14_binary_search.py
@@ -15,22 +15,32 @@
 # print(bin_search([1, 3, 5, 7, 9], 3))
 
 
-def binary_search(arr, target):
-    low = 0
-    high = len(arr) - 1
+# def binary_search(arr, target):
+#     low = 0
+#     high = len(arr) - 1
+#
+#     while low <= high:
+#         mid = (low + high) // 2
+#         guess = arr[mid]
+#         if guess == target:
+#             return mid
+#         elif guess > target:
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+#     return None
+#
+#
+# arr = [1, 3, 5, 7, 9]
+# print(binary_search(arr, 3))  # Output: 1
+# print(binary_search(arr, -1))  # Output: None
 
-    while low <= high:
-        mid = (low + high) // 2
-        guess = arr[mid]
-        if guess == target:
-            return mid
-        elif guess > target:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return None
-
-
-arr = [1, 3, 5, 7, 9]
-print(binary_search(arr, 3))  # Output: 1
-print(binary_search(arr, -1))  # Output: None
+# 14.5 Подсчет меньших чисел
+from bisect import *
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+q = map(int, input().split())
+a.sort()
+for query in q:
+    print(bisect_left(a, query), end=" ")
