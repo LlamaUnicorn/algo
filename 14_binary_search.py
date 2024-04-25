5a3b591d4429401964959d7af9d08fa40ee8b935
LlamaUnicorn <holidaynight@gmail.com>
2024-04-25 05:58:30 +0300

    25.04.2024 14.3 Week: 3

--- 14_binary_search.py
+++ 14_binary_search.py
@@ -22,20 +22,12 @@ from utils import timer
 #     return left < len(array) and array[left] == x
 
 
-@timer
-def bin_search(array, x):
-    left = 0
-    right = len(array) - 1
-    while left < right:
-        mid = (left + right) // 2
-        if array[mid] < x:
-            left = mid + 1
-        else:
-            right = mid
-    return left < len(array) and array[left] == x
-
-print(bin_search([1, 5, 10, 10, 19], 5))
-print(bin_search([1, 5, 8, 10, 20], 4))
+# @timer
+# def bin_search(array, x):
+#
+#
+# print(bin_search([1, 5, 10, 10, 19], 5))
+# print(bin_search([1, 5, 8, 10, 20], 4))
 
 
 
@@ -68,3 +60,20 @@ print(bin_search([1, 5, 8, 10, 20], 4))
 # a.sort()
 # for query in q:
 #     print(bisect_left(a, query), end=" ")
+
+# 14.6 Грузовой лифт в отеле
+from bisect import *
+from itertools import *
+# n = int(input())
+# a = list(map(int, input().split()))
+# m = int(input())
+# q = map(int, input().split())
+n = 4
+a = [10, 10, 10, 10]
+m = 6
+q = [1, 5, 10, 15, 20, 25]
+b = list(accumulate([1] + a))
+for query in q:
+    floor = bisect(b, query)
+    print(floor if floor <= n else -1, end=' ')
+
